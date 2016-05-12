1f181644a8320af36c094b69007bc614e8a1c8bd
pszalach <psz@inf.ethz.ch>
2016-05-12 16:33:37 +0200

    srvcli.

---
+++ apps/tcpscion/srvcli.c
@@ -0,0 +1,13 @@
+#include "lwip/sockets.h"
+#include <pthread.h>
+#include <time.h>
+void * server();
+void * client();
+
+int main(){
+    pthread_t sid, cid;
+    pthread_create(&sid, NULL, &server, NULL);
+    sleep(1);
+    pthread_create(&cid, NULL, &client, NULL);
+    sleep(1);
+}
