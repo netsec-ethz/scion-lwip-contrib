45bc7c0b2b6cb750c71c66fcd3c0df0510a520e1
pszalach <psz@inf.ethz.ch>
2016-06-10 15:09:00 +0200

    file source noted + copyright

--- ports/unix/proj/scion/unixlib.c
+++ ports/unix/proj/scion/unixlib.c
@@ -41,6 +41,23 @@
  * you would like the stack to use.
  */
 /*-----------------------------------------------------------------------------------*/
+/*
+ * Copyright 2016 ETH Zurich
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ * This file is based on ports/unix/proj/lib/unixlib.c
+ */
 #include "lwip/init.h"
 #include "lwip/sys.h"
 #include "lwip/mem.h"
